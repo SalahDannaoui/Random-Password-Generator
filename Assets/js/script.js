8a86e1ab75a2efe9bdc306d2ff29091d7ea09019
Salah <dannaouisalah@gmail.com>
2023-03-01 16:30:22 +1100

    added to readme and removed undefined when user does not confirm generate password

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,65 +12,63 @@ var num = ["0","1","2","3","4","5","6","7","8","9"];
     var possiblechar = [];
     var password = [];
     var userconfirm = window.confirm("Shall we?");
-      if (!userconfirm) {
-        return;
-      }
+    if (userconfirm) {
 
-    var lengthchoice = window.prompt ("Please select your password length: (8-128) characters.");
-    
-    lengthchoice = parseInt(lengthchoice);  
-    
-    while (lengthchoice < 8 || lengthchoice > 128) {
-         alert("sorry, no can do");
-         lengthchoice = window.prompt ("Please select your password length: (8-128) characters.");
-        };
-
-    var capschoice = window.confirm ("Do you want Caps letters?");
+      var lengthchoice = window.prompt ("Please select your password length: (8-128) characters.");
       
-    var lowchoice = window.confirm ("Do you want lowercase letters?");
-    
-    var numchoice = window.confirm ("Do you want numbers?");
-    
-    var specchoice = window.confirm ("Do you want special characters?");
-
-    // add while 
-
-    
-
+      lengthchoice = parseInt(lengthchoice);  
       
-      while (capschoice===false && lowchoice === false && numchoice === false && specchoice === false){
-        alert("You did not select any characters. Please try again.");
-        capschoice = window.confirm ("Do you want Caps letters?");
-        lowchoice = window.confirm ("Do you want lowercase letters?");
-        numchoice = window.confirm ("Do you want numbers?");
-        specchoice = window.confirm ("Do you want special characters?");
-      }
+      while (lengthchoice < 8 || lengthchoice > 128) {
+          alert("sorry, no can do");
+          lengthchoice = window.prompt ("Please select your password length: (8-128) characters.");
+          };
 
-      if (capschoice){
-        possiblechar= possiblechar.concat(capschar)
-      } 
-  
-      if (lowchoice){
-          possiblechar= possiblechar.concat (lowchar)
+      var capschoice = window.confirm ("Do you want Caps letters?");
         
-        }  
-      if (numchoice){
-          possiblechar= possiblechar.concat (num)
+      var lowchoice = window.confirm ("Do you want lowercase letters?");
+      
+      var numchoice = window.confirm ("Do you want numbers?");
+      
+      var specchoice = window.confirm ("Do you want special characters?");
+
+      // add while 
+
+      
+
+        
+        while (capschoice===false && lowchoice === false && numchoice === false && specchoice === false){
+          alert("You did not select any characters. Please try again.");
+          capschoice = window.confirm ("Do you want Caps letters?");
+          lowchoice = window.confirm ("Do you want lowercase letters?");
+          numchoice = window.confirm ("Do you want numbers?");
+          specchoice = window.confirm ("Do you want special characters?");
         }
-  
-      if (specchoice){
-          possiblechar= possiblechar.concat(spechar)
-        }
-  
-        console.log (possiblechar);
-    for (let i = 0; i < lengthchoice; i++) {
-      let randomIndex = Math.floor(Math.random() * possiblechar.length);
-      let randomElement = possiblechar[randomIndex];
-      password.push(randomElement)
+
+        if (capschoice){
+          possiblechar= possiblechar.concat(capschar)
+        } 
     
-    }
+        if (lowchoice){
+            possiblechar= possiblechar.concat (lowchar)
+          
+          }  
+        if (numchoice){
+            possiblechar= possiblechar.concat (num)
+          }
     
-    console.log(password);
+        if (specchoice){
+            possiblechar= possiblechar.concat(spechar)
+          }
+    
+          console.log (possiblechar);
+      for (let i = 0; i < lengthchoice; i++) {
+        let randomIndex = Math.floor(Math.random() * possiblechar.length);
+        let randomElement = possiblechar[randomIndex];
+        password.push(randomElement)
+      
+      }
+      
+      console.log(password);
     
     
   // step one: prompt user //
@@ -81,7 +79,7 @@ var num = ["0","1","2","3","4","5","6","7","8","9"];
   // step two: validate selections//
   //step three: execute function//
   // step four: display password//
-  
+    }
     return password.join('')
   }
 
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## This project enables a user to generate a random password according to criteria they select (length and choice of character types).
 It was also beneficial for learning how to utilise JavaScript to formulate a command that could be replicated.
 
+## Link to Project: https://salahdannaoui.github.io/Random-Password-Generator/
+
 ## Installation
 
 N/A
@@ -12,4 +14,29 @@ N/A
 The generate button, on click, will generate easy to follow prompts on the browser. when answered, the password will be generated and placed in the password box.
 
 ![Screenshot1](./Assets/Images/Screenshot1.png)
+
 ![Screenshot2](./Assets/Images/screenshot2.png)
+
+## License
+
+MIT License
+
+Copyright (c) 2023 salahdannaoui
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
